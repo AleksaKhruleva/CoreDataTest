12136024cc360b2d8ea900e4f8de3390965210dd
Aleksandra Khruleva <adkhruleva@edu.hse.ru>
2024-02-16 17:53:11 +0300

    Core Data example

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Aleksa Khruleva on 15.02.2024.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    private let people: [Person] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
